2845a83b6d950182bbb60f77d96ed418ab08c70d
nmmn231 <dance3237@gmail.com>
2016-06-20 22:25:04 +0800

    animation

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 $(window).scroll(function(){
 	var wScroll = $(this).scrollTop();
-	if(wScroll < 1000){
-		$("#say02").hide();
-	}else if(wScroll >= 1000){
-		$("#say02").fadeIn();
+	if(wScroll >= 1000){
+		$("#say02").css("opacity","1");
 	}
 });
