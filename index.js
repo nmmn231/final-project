527639554f79076deab179d1e0ae2703ce662f7a
nmmn231 <dance3237@gmail.com>
2016-06-21 22:12:41 +0800

    homepage ok

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ $(window).scroll(function(){
 	if(wScroll >= 1500){
 		$("#say02").css("opacity","1");
 	}
+
+    if(wScroll >= 350 && wScroll <= 1000){
+        $("#chang").attr("src","img/chang02.png");
+    }else{
+    	$("#chang").attr("src","img/chang.png");
+    }
+
 	console.log(wScroll);
 });
